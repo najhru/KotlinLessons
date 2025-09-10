ba29659027d75f4ee6b1b64ad3dbfce0e83a5083
najh <>
2025-09-10 16:39:55 +0300

    KS-1-1 solve

---
+++ src/main/kotlin/lesson1/task1.kt
@@ -0,0 +1,9 @@
+package org.example.lesson1
+
+fun main() {
+    val ordersCount: Int = 75
+    val messageCongratulation: String = "Поздравляем с покупкой!"
+
+    println(messageCongratulation)
+    println("Кол-во заказов: $ordersCount")
+}
