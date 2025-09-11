7aad5ffecd5f9d429220e23ab44891be7b0e5168
najh <>
2025-09-11 15:23:06 +0300

    KS-1-1 work on mistakes

--- src/main/kotlin/lesson1/task1.kt
+++ src/main/kotlin/lesson1/task1.kt
@@ -3,7 +3,4 @@ package org.example.lesson1
 fun main() {
     val ordersCount: Int = 75
     val messageCongratulation: String = "Поздравляем с покупкой!"
-
-    println(messageCongratulation)
-    println("Кол-во заказов: $ordersCount")
 }
