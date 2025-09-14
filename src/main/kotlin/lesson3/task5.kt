580c73dc72c7020dec190ee4d1dc17347d712939
najh <najh.ru@gmail.com>
2025-09-14 17:46:51 +0300

    KS-3-5 Задача 5* к Уроку 3

--- src/main/kotlin/lesson3/task5.kt
+++ src/main/kotlin/lesson3/task5.kt
@@ -2,9 +2,13 @@ package org.example.lesson3
 
 fun main() {
     val moveString = "D2-D4;0"
-    val fromCell = moveString.substring(0, 2)
-    val toCell = moveString.substring(3, 5)
-    val move = moveString.substring(6, 7)
+
+    var moveArr = moveString.split(';')
+    val move = moveArr[1]
+
+    moveArr = moveArr[0].split('-')
+    val fromCell = moveArr[0]
+    val toCell = moveArr[1]
 
     println(fromCell)
     println(toCell)
