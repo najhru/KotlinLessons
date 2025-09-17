a95d34ce2377e3a821d11526476c6e652ceb0d6e
najh <najh.ru@gmail.com>
2025-09-17 16:47:06 +0300

    KS-5-5 Задача 1 к Уроку 5 исправление

--- src/main/kotlin/lesson5/task1.kt
+++ src/main/kotlin/lesson5/task1.kt
@@ -1,6 +1,5 @@
 package org.example.lesson5
 
-
 /**
 Задача 1 к Уроку 5
 
@@ -10,6 +9,7 @@ package org.example.lesson5
  */
 const val MIN_VALUE = 1
 const val MAX_VALUE = 10
+
 fun main() {
     val number1 = (MIN_VALUE..MAX_VALUE).random()
     val number2 = (MIN_VALUE..MAX_VALUE).random()
